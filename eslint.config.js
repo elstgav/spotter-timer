32faacc19049c00b9bfedd54e89f7e506e1f631b
Gavin Elster <gavin.elster@me.com>
2024-08-08 11:37:33 -0700

    Configure ESLint
    Add strict typechecking and React rules

--- eslint.config.js
+++ eslint.config.js
@@ -1,24 +1,44 @@
 import js from '@eslint/js'
 import prettier from 'eslint-config-prettier'
+import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import globals from 'globals'
 import tseslint from 'typescript-eslint'
 
 export default tseslint.config({
-  extends: [js.configs.recommended, ...tseslint.configs.recommended],
+  extends: [
+    js.configs.recommended,
+    ...tseslint.configs.strictTypeChecked,
+    ...tseslint.configs.stylisticTypeChecked,
+  ],
+
   files: ['**/*.{ts,tsx}'],
   ignores: ['dist'],
+
   languageOptions: {
     ecmaVersion: 2020,
     globals: globals.browser,
+    parserOptions: {
+      projectService: true,
+      tsconfigRootDir: import.meta.dirname,
+    },
   },
+
   plugins: {
+    react,
     'react-hooks': reactHooks,
     'react-refresh': reactRefresh,
   },
+
+  settings: {
+    react: { version: 'detect' },
+  },
+
   rules: {
     ...prettier.rules,
+    ...react.configs.recommended.rules,
+    ...react.configs['jsx-runtime'].rules,
     ...reactHooks.configs.recommended.rules,
     'react-refresh/only-export-components': [
       'warn',
