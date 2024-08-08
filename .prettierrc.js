6608eb5c944c099baf7045c761bcd49d39e10bcf
Gavin Elster <gavin.elster@me.com>
2024-08-08 10:51:32 -0700

    Install and configure prettier

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+// Prettier auto-formats our code
+// See https://prettier.io/docs/en/options.html for configuration
+
+export default {
+  printWidth: 100,
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'all',
+}
--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,8 @@
 import js from '@eslint/js'
-import globals from 'globals'
+import prettier from 'eslint-config-prettier'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import globals from 'globals'
 import tseslint from 'typescript-eslint'
 
 export default tseslint.config({
@@ -17,10 +18,13 @@ export default tseslint.config({
     'react-refresh': reactRefresh,
   },
   rules: {
+    ...prettier.rules,
     ...reactHooks.configs.recommended.rules,
     'react-refresh/only-export-components': [
       'warn',
-      { allowConstantExport: true },
+      {
+        allowConstantExport: true,
+      },
     ],
   },
 })
