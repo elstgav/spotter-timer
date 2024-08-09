914a5fdad1d60019570084e3cb9024b6e992b6d0
Gavin Elster <gavin.elster@me.com>
2024-08-08 17:50:16 -0700

    Generate types for CSS modules

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,15 @@
 import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
+import sassDts from 'vite-plugin-sass-dts'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [
+    react(), // Support JSX syntax
+    tsconfigPaths(), // Support tsConfig `baseURL` and `paths`
+    sassDts(), // Generates .d.ts files for Sass/CSS
+  ],
   css: {
     modules: {
       localsConvention: 'camelCaseOnly',
