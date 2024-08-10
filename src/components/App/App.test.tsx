3f95723d6f1d81c7ea74b33c29c5f267d7f53652
Gavin Elster <gavin.elster@me.com>
2024-08-10 12:19:08 -0700

    Display timer in App

--- src/components/App/App.test.tsx
+++ src/components/App/App.test.tsx
@@ -1,7 +1,37 @@
-import App from './App'
+import { mockTime } from '$src/tests/mocks/mock-time'
+import { user } from '$src/tests/mocks/mock-user-events'
+import { render, screen, waitFor, within } from '@testing-library/react'
+import { App } from './App'
+
+mockTime.install()
 
 describe('App', () => {
-  it('renders', () => {
-    expect(() => <App />).not.toThrowError()
+  it('does not crash', () => {
+    expect(() => render(<App />)).not.toThrowError()
+  })
+
+  it('displays a timer by default', () => {
+    render(<App />)
+
+    expect(screen.getByRole('timer')).toHaveTextContent('01:00')
+  })
+
+  describe('Timer', () => {
+    it('can be closed and re-opened', async () => {
+      render(<App />)
+      const timerCard = screen.getByRole('region', { name: 'Timer' })
+
+      await user.click(within(timerCard).getByRole('button', { name: /Close/i }))
+
+      await waitFor(() => {
+        expect(screen.queryByRole('region', { name: 'Timer' })).not.toBeInTheDocument()
+      })
+
+      await user.click(screen.getByRole('button', { name: 'Create Timer' }))
+
+      await waitFor(() => {
+        expect(screen.queryByRole('region', { name: 'Timer' })).toBeVisible()
+      })
+    })
   })
 })
--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,3 +1,22 @@
+import { Timer } from '$src/components/Timer'
+import { Box, Button } from '@mui/material'
+import { useState } from 'react'
+import { main } from './App.module.scss'
+
 export const App = () => {
-  return <>Hello World</>
+  const [open, setOpen] = useState(true)
+
+  const onTimerOpenClick = () => setOpen(true)
+
+  return (
+    <Box className={main} component="main">
+      {open ? (
+        <Timer onClose={() => setOpen(false)} />
+      ) : (
+        <Button variant="outlined" color="secondary" onClick={onTimerOpenClick}>
+          Create Timer
+        </Button>
+      )}
+    </Box>
+  )
 }
